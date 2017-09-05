faaf7379abc987cacdea6ee49a40cc472b5934d8
windbamboo <windbamboo@163.com>
2017-09-05 20:15:15 +0800

    Initial commit

---
+++ pc-web/src/main/java/com/ronglaobao/App.java
@@ -0,0 +1,13 @@
+package com.ronglaobao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
